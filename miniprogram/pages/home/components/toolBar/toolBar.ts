4b6b05bb5648d11b045cb7730670eedf5d90c793
arleyGuoLei <34805850@qq.com>
2022-02-21 23:05:58 +0800

    feat: 首页添加我的小程序提示 提示卡点击提示

--- miniprogram/pages/home/components/toolBar/toolBar.ts
+++ miniprogram/pages/home/components/toolBar/toolBar.ts
@@ -1,3 +1,4 @@
+import { toast } from './../../../../utils/util'
 
 App.Component({
   methods: {
@@ -6,6 +7,8 @@ App.Component({
       const bookSelect = (page[page.length - 1]?.selectComponent('#book-select'))
       bookSelect?.show()
     },
-    onTipCard () {}
+    onTipCard () {
+      void toast.show('可用于「对战模式」和「每日词汇」助力选择', 0, 640)
+    }
   }
 })
