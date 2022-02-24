ce0e0451bdaff42359d52a7847dba50885640ddd
arley <34805850@qq.com>
2022-02-24 21:31:38 +0800

    feat: 上线

--- miniprogram/utils/config.ts
+++ miniprogram/utils/config.ts
@@ -1,7 +1,7 @@
 export default {
   cloudEnv: {
-    develop: 'cloud1-2gxt3f0qb7420723',
-    release: 'release-5g3qji4cea00e725'
+    develop: 'dev-lkupx',
+    release: 'prod-words-pk'
   },
   /** 对战模式每道题目的选项数目 */
   combatOptionNumber: 4,
